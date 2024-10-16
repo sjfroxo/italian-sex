d77347734f32d0572eea2c16b139309e413c82c2
Alexey Kholyavski <kholyavski@mail.ru>
2024-10-16 16:58:58 +0300

    add index pages

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        return view('main.index');
+    }
+}
+
