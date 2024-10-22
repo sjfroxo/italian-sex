75f8ea66a5ffbeb723a01b5580422734f3227491
Alexey Kholyavski <kholyavski@mail.ru>
2024-10-22 16:59:18 +0300

    add admin panel gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,12 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Enums\RoleEnum;
+use App\Models\User;
+//use App\Policies\AdminPolicy;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+//        User::class => AdminPolicy::class,
     ];
 
     /**
@@ -21,6 +26,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+//        $this->registerPolicies();
+//        Gate::define('accessAdminPanel', [AdminPolicy::class, 'accessAdminPanel']);
+        Gate::define('accessAdminPanel', function (User $user) {
+            return $user->role_id == RoleEnum::ADMIN->value
+                ? Response::allow()
+                : Response::deny('Вы должны быть администратором.');
+        });
     }
 }
