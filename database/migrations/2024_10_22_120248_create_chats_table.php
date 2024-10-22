f5c6b9553fd3b6ad940ce5ca26bb8f7cde79a063
Alexey Kholyavski <kholyavski@mail.ru>
2024-10-22 17:14:16 +0300

    add migrations

---
+++ database/migrations/2024_10_22_120248_create_chats_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable(); // Название чата, например, для групповых чатов
+            $table->timestamps();
+        });
+
+        // Создание промежуточной таблицы для связи многие ко многим между пользователями и чатами
+        Schema::create('chat_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('chat_id')->constrained('chats')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
